a442d5f6908639845b685d1a44180471de372d7b
bless727 <bless727@gmail.com>
2019-11-09 15:12:36 -0500

    Add Postsgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,11 +77,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite',
+        'USER':'postgres',
+        'PASSWORD':'Sidewalk727',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
+
     }
+   
 }
 
 
