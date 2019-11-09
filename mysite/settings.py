33cccd81db50765c27d4c91d65cb848b3363769a
bless727 <bless727@gmail.com>
2019-11-09 15:53:57 -0500

    Added Static Files

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, './mysite/static/')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'mysite/static'),
+    os.path.join(BASE_DIR, './mysite/static'),
 )
