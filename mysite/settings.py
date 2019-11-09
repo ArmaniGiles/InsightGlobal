e9540e5ae88a0525bc263ae3e41ed776f2fb44b9
bless727 <bless727@gmail.com>
2019-11-09 15:58:18 -0500

    Added Static Files

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, './mysite/static/')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, './mysite/static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, './mysite/static'),
+# )
