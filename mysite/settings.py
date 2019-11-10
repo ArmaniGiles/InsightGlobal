772920d625f7ab39e21ef04084a76d1d0e9d3539
bless727 <bless727@gmail.com>
2019-11-10 15:10:29 -0500

    A

--- mysite/settings.py
+++ mysite/settings.py
@@ -155,7 +155,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # prod_db  =  dj_database_url.config(default='postgres://fiutbcxoyxeuqe:f23b61665dd19170ad33a4357e8f6c9d42452ba6657b8430d459efa9e5f48ec0@ec2-107-20-167-241.compute-1.amazonaws.com:5432/d7cht05qse4g2i')
 prod_db  =  dj_database_url.config()
 
-print('prod_db ', prod_db)
+# print('prod_db ', prod_db)
 DATABASES['default'].update(prod_db)
 # del DATABASES['default']['OPTIONS']['sslmode']
 # try to load local_settings.py if it exists
