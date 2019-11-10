dc8aa76d1474f28f850a4902bc0f2891ba26a4ee
bless727 <bless727@gmail.com>
2019-11-10 13:48:15 -0500

    A

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,15 +21,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'i%06y2q&4l-!nv*8oolv470b!o)!xg*^9f7^d=q10#b$wd%c_e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # export SECRET_KEY='847e44e26a02eee37a3ee0071629844b36c2ee273691d6a6'
 # export DEBUG_VALUE="True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # ALLOWED_HOSTS = ['ancient-meadow-75191.mysite.com']
 # ALLOWED_HOSTS = ['ancient-meadow-75191.herokuapp.com']
 # ALLOWED_HOSTS = ['0.0.0.0', 'localhost','bless727.herokuapp.com']
-ALLOWED_HOSTS = ['127.0.0.1','bless727.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','bless727.herokuapp.com']
 
 
 
