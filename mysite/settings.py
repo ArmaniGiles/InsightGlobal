128bebf78e5fa9cbce202fc45dfd1c09a1d7db92
bless727 <bless727@gmail.com>
2019-11-10 19:31:13 -0500

    A

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,8 +54,8 @@ INSTALLED_APPS = [
 #dj-database-url==0.4.0
  
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
