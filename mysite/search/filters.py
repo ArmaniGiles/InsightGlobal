31dc0136a1891bc9f3c449f6a90577b2f1d4e963
bless727 <bless727@gmail.com>
2019-11-11 22:46:52 -0500

    Finishing Touches

--- mysite/search/filters.py
+++ mysite/search/filters.py
@@ -1,13 +1,9 @@
-from django import forms
-from django.contrib.auth.models import User, Group
 from .models import Zipcode
-
 import django_filters
 
 
-class UserFilter(django_filters.FilterSet):
+class ZipCodeFilter(django_filters.FilterSet):
     JURISDICTION_NAME = django_filters.CharFilter(lookup_expr='icontains')
-    # groups = django_filters.ModelMultipleChoiceFilter(queryset=Group.objects.all(), widget=forms.CheckboxSelectMultiple)
 
     class Meta:
         model = Zipcode
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,11 +4,11 @@ from django.views.generic import TemplateView
 
 from django_filters.views import FilterView
 
-from mysite.search.filters import UserFilter
+from mysite.search.filters import ZipCodeFilter
 
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^search/$', FilterView.as_view(filterset_class=UserFilter, template_name='search/user_list.html'), name='search'),
+    url(r'^search/$', FilterView.as_view(filterset_class=ZipCodeFilter, template_name='search/user_list.html'), name='search'),
     url(r'^admin/', include(admin.site.urls)),
 ]
